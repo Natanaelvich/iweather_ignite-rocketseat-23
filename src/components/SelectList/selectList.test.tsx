ff819fb53b7915a1f14f1e72c935938411418003
natanaelvich <taelima1997@gmail.com>
2023-07-27 23:01:42 -0300

    update tests component selectlist

--- src/components/SelectList/selectList.test.tsx
+++ src/components/SelectList/selectList.test.tsx
@@ -1,24 +1,41 @@
-import { render, screen } from "@testing-library/react-native"
+import { render, screen, fireEvent } from "@testing-library/react-native"
 
 import { SelectList } from '@components/SelectList'
-import { CityProps } from "@services/getCityByNameService"
 
 describe("Component: SelectList", () =>{
   it('should be return city details selected', async() => {
     const data = [
       { id: '1', name: 'Campinas', latitude: 123, longitude: 456 },
-      { id: '2', name: 'Campo grande', latitude: 123, longitude: 456 }
+      { id: '2', name: 'Campo grande', latitude: 789, longitude: 487 }
     ]
+
+    const onPress = jest.fn();
 
     render(
       <SelectList 
         data={data}
         onChange={() =>{}}
-        onPress={() => {}}
+        onPress={onPress}
       />
     )
 
-    const selectedCity = screen.getByText('Campinas')
-    console.log(selectedCity)
+    const selectedCity = screen.getByText(/campo/i)
+    fireEvent.press(selectedCity)
+
+    expect(onPress).toBeCalledWith(data[1])
+  })
+
+  it('not should be show options when data props is empty', () => {
+    render(
+      <SelectList 
+        data={[]}
+        onChange={() => {}}
+        onPress={()=>{}}
+      />
+    )
+
+    const options = screen.getByTestId('options')
+
+    expect(options.children).toHaveLength(0)
   })
 })

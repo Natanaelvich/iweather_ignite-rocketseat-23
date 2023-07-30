c3f5f3670045657c425f1d23d4f53e77b4784334
natanaelvich <taelima1997@gmail.com>
2023-07-30 09:24:10 -0300

    add more tests for service getCityByNameService

--- src/services/getCityByNameService.test.ts
+++ src/services/getCityByNameService.test.ts
@@ -1,15 +1,36 @@
-import { mockCityAPIResponse } from '@__tests__/mocks/api/mockCityAPIResponse'
-import { api } from './api'
-import { getCityByNameService } from './getCityByNameService'
+import { mockCityAPIResponse } from "@__tests__/mocks/api/mockCityAPIResponse";
+import { api } from "./api";
+import { getCityByNameService } from "./getCityByNameService";
 
 describe("Service: getCityByNameService", () => {
-    it('should return city details', async () => {
-  
-  
-      jest.spyOn(api, "get").mockResolvedValue({ data: mockCityAPIResponse })
+  it("should return city details", async () => {
+    jest.spyOn(api, "get").mockResolvedValue({ data: mockCityAPIResponse });
 
-    const response = await getCityByNameService('São Paulo')
+    const response = await getCityByNameService("São Paulo");
 
-    expect(response.length).toBeGreaterThan(0)
-  })
-})
+    expect(response.length).toBeGreaterThan(0);
+  });
+
+  it("should return empty array if city is not found", async () => {
+    jest.spyOn(api, "get").mockRejectedValue({});
+
+    const response = await getCityByNameService("Cidade que não existe");
+
+    expect(response.length).toBe(0);
+  });
+
+  it("should return name withou country if country is not found", async () => {
+    const newMock = {
+      ...mockCityAPIResponse,
+      sys: {
+        country: undefined,
+      },
+    };
+
+    jest.spyOn(api, "get").mockResolvedValue({ data: newMock });
+
+    const response = await getCityByNameService("São Paulo");
+
+    expect(response[0].name).toBe("São Paulo");
+  });
+});
